91382be0fde3c1c8a17b9ba725a2210402b06b59
Dmitrii Sapegin <lejeboka24@gmail.com>
2022-06-02 15:33:35 +0300

    prj 5 started

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,15 @@
+// popupProfile
+
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
-
 let profilePageName = document.querySelector('.profile__name');
 let profilePageTitle = document.querySelector('.profile__title');
-
 let inputName = document.querySelector('.popup__input-name');
 let inputTitle = document.querySelector('.popup__input-title');
 
 let popupForm = document.querySelector('.popup__info-form');
 
-/*
-let likeButton = document.querySelector('.place__like-button');
-let likeButtons = document.querySelectorAll('.place__like-button');
-let likeButtonActive = document.querySelector('.place__like-button_active');
-
-
-likeButtons.forEach((likeButton) function() {
-  likeButton.addEventListener('click', function() {
-    likeButton.classList.remove('place__like-button');
-    likeButton.classList.add('place__like-button_active');
-  })
-});
-*/
 editButton.addEventListener('click', function() {
   let currentName = profilePageName.textContent;
   let currentTitle = profilePageTitle.textContent;
@@ -44,4 +31,52 @@ closeButton.addEventListener('click', function() {
   popup.classList.remove('popupOpen');
 }
 )
+
+// popupAdd
+
+let popupAdd = document.querySelector('.popupAdd');
+let addButton = document.querySelector('.profile__add-button');
+
+let placeName = document.querySelector('.place__name');
+let placePhoto = document.querySelector('.place__photo');
+
+let popupAddForm = document.querySelector('.popupAdd__info-form');
+
+let inputAddName = document.querySelector('.popupAdd__input-place-name');
+let inputAddPhoto = document.querySelector('.popupAdd__input-place-link');
+
+let closeAddButton = document.querySelector('.popupAdd__close-button');
+
+addButton.addEventListener('click', function() {
+  popupAdd.classList.add('popupAddOpen');
+}
+)
+
+popupAddForm.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  popupAdd.classList.remove('popupAddOpen');
+}
+)
+
+closeAddButton.addEventListener('click', function() {
+  popupAdd.classList.remove('popupAddOpen');
+}
+)
+
+
+
+
+/*
+let likeButton = document.querySelector('.place__like-button');
+let likeButtons = document.querySelectorAll('.place__like-button');
+let likeButtonActive = document.querySelector('.place__like-button_active');
+
+
+likeButtons.forEach((likeButton) function() {
+  likeButton.addEventListener('click', function() {
+    likeButton.classList.remove('place__like-button');
+    likeButton.classList.add('place__like-button_active');
+  })
+});
+*/
 
