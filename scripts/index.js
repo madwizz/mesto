eb38eb2008d366c454f142baa3c6410a381ede43
Dmitrii Sapegin <lejeboka24@gmail.com>
2022-06-06 11:17:34 +0300

    js fixes

--- scripts/index.js
+++ scripts/index.js
@@ -7,85 +7,65 @@ const profilePageName = document.querySelector('.profile__name');
 const profilePageTitle = document.querySelector('.profile__title');
 const inputName = document.querySelector('.popup__input-name');
 const inputTitle = document.querySelector('.popup__input-title');
-
 const popupForm = document.querySelector('.popup__info-form');
+
+// popupAdd
+
+const popupAdd = document.querySelector('.popupAdd');
+const addButton = document.querySelector('.profile__add-button');
+const popupAddForm = document.querySelector('.popupAdd__add-form');
+const inputAddName = document.querySelector('.popupAdd__input-place-name');
+const inputAddPhoto = document.querySelector('.popupAdd__input-place-link');
+const closeAddButton = document.querySelector('.popupAdd__close-button');
+
+// submit + addNewPlace
+
+const placesContainer = document.querySelector('.places');
+
+// popupPhotoClose
+
+const closePopupPhoto = document.querySelector('.popupPhoto__close-button');
+
+// popupPhotoOpen
+
+const popupImage = document.querySelector('.popupPhoto__image');
+const popupPhoto = document.querySelector('.popupPhoto');
+const placeName = document.querySelector('.place__name');
+
+// activate buttons for newPlaces
+
+const places = document.querySelectorAll('.place');
 
 // initialPlaces
 
 const initialPlaces = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   },
   {
     name: 'Холмогорский район',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
   }
 ];
 
-// popupAdd
-
-const popupAdd = document.querySelector('.popupAdd');
-const addButton = document.querySelector('.profile__add-button');
-
-const placeName = document.querySelector('.place__name');
-const placePhoto = document.querySelector('.place__photo');
-
-const popupAddForm = document.querySelector('.popupAdd__add-form');
-
-const inputAddName = document.querySelector('.popupAdd__input-place-name');
-const inputAddPhoto = document.querySelector('.popupAdd__input-place-link');
-
-const closeAddButton = document.querySelector('.popupAdd__close-button');
-
-addButton.addEventListener('click', () => {
-  popupAdd.classList.add('popupAddOpen');
-}
-);
-
-// submit + addNewPlace
-
-const placesContainer = document.querySelector('.places');
-
-popupAddForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-
-  addPlace(inputAddName.value, inputAddPhoto.value);
-
-  popupAdd.classList.remove('popupAddOpen');
-}
-)
-
-closeAddButton.addEventListener('click', () => {
-  popupAdd.classList.remove('popupAddOpen');
-}
-)
-
-// addPlace six times on start-up
-
-for (let i = 0; i < initialPlaces.length; i++) {
-  addPlace(initialPlaces[i].name, initialPlaces[i].link);
-}
-
-
 // addPlace on start-up
-
 
 function addPlace(name, link) {
 
@@ -93,53 +73,13 @@ function addPlace(name, link) {
 
   newPlace.querySelector('.place__name').textContent = name;
   newPlace.querySelector('.place__photo').src = link;
-
   
   addPlaceListeners(newPlace);
 
-  placesContainer.append(newPlace);
+  placesContainer.prepend(newPlace);
 }
 
-// popupPhotoOpen
-
-const popupImage = document.querySelector('.popupPhoto__image');
-const popupPhoto = document.querySelector('.popupPhoto');
-
-// popupPhotoClose
-
-const closePopupPhoto = document.querySelector('.popupPhoto__close-button');
-
-closePopupPhoto.addEventListener('click', () => {
-  popupPhoto.classList.remove('popupPhotoOpen');
-}
-);
-
-// editButton
-
-editButton.addEventListener('click', () => {
-  let currentName = profilePageName.textContent;
-  let currentTitle = profilePageTitle.textContent;
-  inputName.value = currentName;
-  inputTitle.value = currentTitle;
-  popup.classList.add('popupOpen');
-}
-)
-
-popupForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  profilePageName.textContent = inputName.value;
-  profilePageTitle.textContent = inputTitle.value;
-  popup.classList.remove('popupOpen');
-}
-)
-
-closeButton.addEventListener('click', () => {
-  popup.classList.remove('popupOpen');
-}
-)
-
-
-//likeButton & deleteButton
+//likeButton, deleteButton & popupPhoto
 
 function addPlaceListeners (place) {
   const deletePlaceButton = place.querySelector('.place__delete-button');
@@ -162,46 +102,82 @@ function addPlaceListeners (place) {
   );
 }
 
-const places = document.querySelectorAll('.place');
+// activate buttons for newPlaces
 
 for (let i = 0; i < places.length; i ++) {
   addPlaceListeners(places[i]);
 }
 
-// forEach( (place) => addPlaceListeners(place)); not legal
-
-// Array.from(document.querySelectorAll('.place')).forEach(addPlaceListeners);
-
-
-
-
-// placeTemplate
-
 /*
-function addPlace (photoValue, titleValue) {
+ forEach( (place) => addPlaceListeners(place)); not legal
+ Array.from(document.querySelectorAll('.place')).forEach(addPlaceListeners);
+*/
 
+// addPlace six times on start-up
+
+for (let i = 0; i < initialPlaces.length; i++) {
+  addPlace(initialPlaces[i].name, initialPlaces[i].link);
 }
 
-const placeTemplate = document.querySelector('#place').content;
+// open popupAdd button
 
-const place = placeTemplate.querySelector('.place').cloneNode(true);
-const placePhoto = place.querySelector('.place__photo');
-const placeTitle = place.querySelector('.place__title');
-placePhoto
+addButton.addEventListener('click', () => {
+  popupAdd.classList.add('popupAddOpen');
+}
+);
 
-placePhoto.querySelector('.place__photo').src = '#'
-placeTitle.querySelector('.place__title').textContent = '#'
+// submit button + addNewPlace
 
-places.append(placeElement);
+popupAddForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
 
+  addPlace(inputAddName.value, inputAddPhoto.value);
 
-// likeButton
+  popupAdd.classList.remove('popupAddOpen');
+}
+);
 
-let likeButton = document.querySelector('.place__like-button');
-let likeButtons = document.querySelectorAll('.place__like-button');
-let likeButtonActive = document.querySelector('.place__like-button_active');
+// close popupAdd button
 
-*/
+closeAddButton.addEventListener('click', () => {
+  popupAdd.classList.remove('popupAddOpen');
+}
+);
+
+// popupPhotoClose
+
+closePopupPhoto.addEventListener('click', () => {
+  popupPhoto.classList.remove('popupPhotoOpen');
+}
+);
+
+// open editPopup button
+
+editButton.addEventListener('click', () => {
+  let currentName = profilePageName.textContent;
+  let currentTitle = profilePageTitle.textContent;
+  inputName.value = currentName;
+  inputTitle.value = currentTitle;
+  popup.classList.add('popupOpen');
+}
+);
+
+// submit button + profileInfoEdit
+
+popupForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  profilePageName.textContent = inputName.value;
+  profilePageTitle.textContent = inputTitle.value;
+  popup.classList.remove('popupOpen');
+}
+);
+
+// close editPopup button
+
+closeButton.addEventListener('click', () => {
+  popup.classList.remove('popupOpen');
+}
+);
 
 
 
