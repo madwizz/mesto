b2e30f50a6f85f94d9788024314085a6e3bb5fb5
Dmitrii Sapegin <lejeboka24@gmail.com>
2022-06-14 11:15:41 +0300

    popup reused & edited

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,30 @@
-// popupProfile
+// popupEdit
 
-const popupProfile = document.querySelector('.popup-profile');
+const popupEdit = document.querySelector('.popup_edit');
 const editProfileButton = document.querySelector('.profile__edit-button');
-const closeProfileButton = document.querySelector('.popup-profile__close-button');
+const closeButton = document.querySelector('.popup__close-button');
 const profilePageName = document.querySelector('.profile__name');
 const profilePageTitle = document.querySelector('.profile__title');
 const inputProfileName = document.querySelector('.popup-profile__info_input-name');
 const inputProfileTitle = document.querySelector('.popup-profile__info_input-title');
-const popupProfileForm = document.querySelector('.popup-profile__info-form');
+const popupEditForm = document.querySelector('.popup__form_edit');
 
 // popupAdd
 
-const popupAdd = document.querySelector('.popup-add');
+const popupAdd = document.querySelector('.popup_add');
 const addButton = document.querySelector('.profile__add-button');
-const popupAddForm = document.querySelector('.popup-add__form');
+const popupAddForm = document.querySelector('.popup__form_add');
 const inputAddName = document.querySelector('.popup-add__info_input-place-name');
 const inputAddPhoto = document.querySelector('.popup-add__info_input-place-link');
-const closeAddButton = document.querySelector('.popup-add__close-button');
 
 // submit + addNewPlace
 
 const placesContainer = document.querySelector('.places');
 
-// popupPhotoClose
-
-const closePopupPhoto = document.querySelector('.popup-photo__close-button');
-
 // popupPhotoOpen
 
-const popupImage = document.querySelector('.popup-photo__image');
-const popupPhoto = document.querySelector('.popup-photo');
+const popupImage = document.querySelector('.popup__photo-image');
+const popupPhoto = document.querySelector('.popup_photo');
 
 // activate buttons for newPlaces
 
@@ -60,6 +55,15 @@ function addPlace(card) {
   placesContainer.prepend(card);
 }
 
+// open + close popup function
+
+const openPopup = (popup) => {
+  popup.classList.add('popup_opened');
+}
+const closePopup = (popup) => {
+  popup.classList.remove('popup_opened');
+}
+
 //likeButton, deleteButton & popupPhoto
 
 function addPlaceListeners(place) {
@@ -74,10 +78,10 @@ function addPlaceListeners(place) {
   }
   );
   const placeImage = place.querySelector('.place__photo');
-  const popupPhotoName = document.querySelector('.popup-photo__name');
+  const popupPhotoName = document.querySelector('.popup__photo-name');
   const placeName = place.querySelector('.place__name');
   placeImage.addEventListener('click', () => {
-    popupPhoto.classList.add('popup-photo_open');
+    openPopup(popupPhoto);
     popupImage.src = placeImage.src;
     popupImage.alt = placeImage.alt;
     popupPhotoName.textContent = placeName.textContent;
@@ -93,66 +97,56 @@ for (let i = 0; i < initialPlaces.length; i++) {
   addPlace(place);
 }
 
-// open popupAdd button
+// open + close popupAdd buttons
 
 addButton.addEventListener('click', () => {
-  popupAdd.classList.add('popup-add_open');
+  openPopup(popupAdd);
+}
+);
+closeButton.addEventListener('click', () => {
+  closePopup(popupAdd);
 }
 );
 
-// submit button + addNewPlace
+// popupPhotoClose
+
+closeButton.addEventListener('click', () => {
+  closePopup(popupPhoto);
+}
+);
+
+// submit popupAdd button + addNewPlace
 
 function addFormSubmitHandler(evt) {
   evt.preventDefault();
   const newPlace = createPlace(inputAddName.value, inputAddPhoto.value);
   addPlaceListeners(newPlace);
   addPlace(newPlace);
-  popupAdd.classList.remove('.popup-add_open');
+  popupClose(popupAdd);
   popupAddForm.reset();
 }
 popupAddForm.addEventListener('submit', addFormSubmitHandler);
 
-// close popupAdd button
-
-closeAddButton.addEventListener('click', () => {
-  popupAdd.classList.remove('popup-add_open');
-}
-);
-
-// popupPhotoClose
-
-closePopupPhoto.addEventListener('click', () => {
-  popupPhoto.classList.remove('popup-photo_open');
-}
-);
-
-// open editPopup button
+// open + close popupEdit buttons
 
 editProfileButton.addEventListener('click', () => {
   const currentName = profilePageName.textContent;
   const currentTitle = profilePageTitle.textContent;
   inputProfileName.value = currentName;
   inputProfileTitle.value = currentTitle;
-  popupProfile.classList.add('popup-profile_open');
+  openPopup(popupEdit);
 }
 );
+closeButton.addEventListener('click', () => {
+  closePopup(popupEdit);
+})
 
-// submit button + profileInfoEdit
+// submit popupEdit button
 
-popupProfileForm.addEventListener('submit', (evt) => {
+popupEditForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   profilePageName.textContent = inputProfileName.value;
   profilePageTitle.textContent = inputProfileTitle.value;
-  popupProfile.classList.remove('popup-profile_open');
+  closePopup(popupEdit);
 }
 );
-
-// close editPopup button
-
-closeProfileButton.addEventListener('click', () => {
-  popupProfile.classList.remove('popup-profile_open');
-}
-);
-
-
-
