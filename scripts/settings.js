35b28b0809670e135756ff8adc196a9878a6c0d3
Dmitrii Sapegin <lejeboka24@gmail.com>
2022-06-22 11:47:47 +0300

    settings fixed

--- scripts/settings.js
+++ scripts/settings.js
@@ -2,7 +2,7 @@ const settings = {
   formSelector: ".popup__form",
   inputSelector: ".popup__info",
   submitButtonSelector: ".popup__submit-button",
-  inactiveButtonClass: ".popup__submit-button_disabled",
-  inputErrorClass: ".popup__info_invalid",
-  errorClass: ".popup__error-message_active",
+  inactiveButtonClass: "popup__submit-button_disabled",
+  inputErrorClass: "popup__info_invalid",
+  errorClass: "popup__error-message_active",
 };
