c1920e152aea818659473c232647c7c92ca2f450
Dmitrii Sapegin <lejeboka24@gmail.com>
2022-07-26 18:44:54 +0300

    webpack i-ed

---
+++ src/index.js
@@ -0,0 +1,154 @@
+import FormValidation from "../scripts/FormValidation.js";
+import Card from "../scripts/Card.js";
+import { initialPlaces } from "../scripts/initialPlaces.js";
+import { settings } from "../scripts/settings.js";
+import Section from "../scripts/Section.js";
+import UserInfo from "../scripts/UserInfo.js";
+import PopupWithForm from "../scripts/PopupWithForm.js";
+import PopupWithImage from "../scripts/PopupWithImage.js";
+import "./index.css";
+
+import {
+  popupEdit,
+  editProfileButton,
+  closeEdit,
+  profilePageName,
+  profilePageTitle,
+  inputProfileName,
+  inputProfileTitle,
+  popupEditForm,
+  popupAdd,
+  addButton,
+  popupAddForm,
+  // inputAddName,
+  // inputAddPhoto,
+  closeAdd,
+  placesContainer,
+  // popupImage,
+  popupPhoto,
+  closePhoto,
+  // popupPhotoName,
+  // popups,
+} from "../scripts/variables.js";
+
+// photo popup func
+
+function handleImageClick(name, src) {
+  popupImage.open(name, src);
+}
+
+// addPlace to the page; places = placesContainer
+
+// function addPlace(places, place) {
+//   const card = createPlace(place);
+//   places.prepend(card);
+// }
+
+// createPlace
+
+function createPlace(cardData) {
+  const card = new Card(cardData, handleImageClick, "#place");
+  return card.generateCard();
+}
+
+// load six places
+
+const renderInitialPlaces = new Section(
+  {
+    items: initialPlaces,
+    renderer: (cardData) => {
+      const cardElement = createPlace(cardData);
+      renderInitialPlaces.addItem(cardElement);
+    },
+  },
+  placesContainer
+);
+
+renderInitialPlaces.renderItem();
+
+const profileInfo = new UserInfo({
+  profileName: profilePageName,
+  profileTitle: profilePageTitle,
+});
+
+const profileSubmit = (userInfo) => {
+  console.log(userInfo);
+  profileInfo.setUserInfo(userInfo);
+};
+
+function placeSubmit(obj) {
+  const place = createPlace(obj);
+  renderInitialPlaces.addItem(place);
+  popupAddPlace.close();
+}
+
+// + addNewPlace
+
+// function addFormSubmitHandler(evt) {
+//   evt.preventDefault();
+//   addPlace(placesContainer, {
+//     name: inputAddName.value,
+//     link: inputAddPhoto.value,
+//   });
+//   closePopup(popupAdd);
+//   popupAddForm.reset();
+//   validAddForm.resetValidation();
+// }
+
+// open + close popupAdd buttons + validation before input
+
+addButton.addEventListener("click", () => {
+  popupAddPlace.open();
+});
+closeAdd.addEventListener("click", () => {
+  popupAddPlace.close();
+  popupAddForm.reset();
+  validAddForm.resetValidation();
+});
+
+// popupPhotoClose
+
+closePhoto.addEventListener("click", () => {
+  popupImage.close();
+});
+
+// submit popupAdd button
+
+// popupAddForm.addEventListener("submit", placeSubmit);
+
+// open + close popupEdit buttons + validation w/ profile value
+
+editProfileButton.addEventListener("click", () => {
+  inputProfileName.value = profilePageName.textContent;
+  inputProfileTitle.value = profilePageTitle.textContent;
+  popupProfile.open();
+});
+closeEdit.addEventListener("click", () => {
+  popupProfile.close();
+  popupEditForm.reset();
+  validEditForm.resetValidation();
+});
+
+// submit popupEdit button
+
+// popupEditForm.addEventListener("submit", (evt) => {
+//   evt.preventDefault();
+//   profilePageName.textContent = inputProfileName.value;
+//   profilePageTitle.textContent = inputProfileTitle.value;
+//   popupProfile.close();
+// });
+
+const validEditForm = new FormValidation(settings, popupEditForm);
+validEditForm.enableValidation();
+
+const validAddForm = new FormValidation(settings, popupAddForm);
+validAddForm.enableValidation();
+
+const popupProfile = new PopupWithForm(popupEdit, profileSubmit);
+popupProfile.setEventListeners();
+
+const popupImage = new PopupWithImage(popupPhoto);
+popupImage.setEventListeners();
+
+const popupAddPlace = new PopupWithForm(popupAdd, placeSubmit);
+popupAddPlace.setEventListeners();
